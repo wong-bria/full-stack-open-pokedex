e7f762316f1b2af4a6ff65ade8368ef7d6cdd112
wong-bria <wong.bria@northeastern.edu>
2026-02-26 21:39:34 -0500

    added additional end-to-end test

--- e2e-tests/pokedex.spec.ts
+++ e2e-tests/pokedex.spec.ts
@@ -2,8 +2,14 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-     await page.goto('')
+    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
+  })
+
+  test('can navigate from main main to page about ivysaur and see chlorophyll', async ({ page }) => {
+    await page.goto('')
+    await page.getByText('ivysaur').click()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
   })
 })
